a1c16f31881ce3fe963a704b3bc8ae431bec1d20
sbaird22 <shanebaird30@yahoo.com>
2025-01-09 10:30:47 -0700

    setting up json

--- client/src/pages/Services.tsx
+++ client/src/pages/Services.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import SEO from '../components/SEO';
 import ServiceCard from '../components/ServiceCard';
 import { fetchServices } from '../api';
